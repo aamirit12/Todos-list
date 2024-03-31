726d95afe02ae69cfbc3cc4c93d77759455f97f9
aamirit12 <aamiritcorner786@gmail.com>
2024-03-31 10:11:51 +0500

    my todo list

---
+++ app.js
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        { message: "Add the items in the list",
+            type: 'input',
+            name: 'TodoItems'
+        },
+        { name: "addMore",
+            message: "Do you want to add more in the list",
+            type: 'confirm',
+            default: "false",
+        }
+    ]);
+    todos.push(addTask.TodoItems);
+    condition = addTask.addMore;
+    console.log(todos);
+}
